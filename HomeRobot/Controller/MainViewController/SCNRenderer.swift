d0da99836aa369e26f8bb8262cc2c8168d0d51f7
ferdinandl007 <ferdinandloesch@me.com>
2020-01-24 14:25:38 +0100

    up

--- HomeRobot/Controller/MainViewController/SCNRenderer.swift
+++ HomeRobot/Controller/MainViewController/SCNRenderer.swift
@@ -9,76 +9,78 @@
 import Foundation
 import ARKit
 
-//extension ViewController: ARSCNViewDelegate {
-//   // MARK: - ARSCNViewDelegate
-//
-//   // Override to create and configure nodes for anchors added to the view's session.
-//   func renderer(_: SCNSceneRenderer, nodeFor _: ARAnchor) -> SCNNode? {
-//       // if planesVizNodes.count > 0 { return nil; }
-//       let node = SCNNode()
-//       return node
-//   }
-//
-//   func renderer(_: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-//       guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-//
-//       // Create a SceneKit plane to visualize the plane anchor using its position and extent.
-//       let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-//       plane.firstMaterial?.diffuse.contents = UIColor.magenta
-//
-//       let planeNode = SCNNode(geometry: plane)
-//       planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
-//
-//       planesVizNodes[anchor.identifier] = node // keep track of plane nodes so you can move them once you localize to a new map.
-//
-//       /*
-//        `SCNPlane` is vertically oriented in its local coordinate space, so
-//        rotate the plane to match the horizontal orientation of `ARPlaneAnchor`.
-//        */
-//       planeNode.eulerAngles.x = -.pi / 2
-//
-//       // Make the plane visualization semitransparent to clearly show real-world placement.
-//       // planeNode.opacity = 0.012
-//       planeNode.opacity = 0.12
-//
-//       /*
-//        Add the plane visualization to the ARKit-managed node so that it tracks
-//        changes in the plane anchor as plane estimation continues.
-//        */
-//       node.addChildNode(planeNode)
-////       voxelMap.updateGroundPlane(planeAnchor)
-//   }
-//
-//   func renderer(_: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-//       // Update content only for plane anchors and nodes matching the setup created in `renderer(_:didAdd:for:)`.
-//       guard let planeAnchor = anchor as? ARPlaneAnchor,
-//           let planeNode = node.childNodes.first,
-//           let plane = planeNode.geometry as? SCNPlane
-//       else { return }
-//       
-////       voxelMap.updateGroundPlane(planeAnchor)
-//
-//       // Plane estimation may shift the center of a plane relative to its anchor's transform.
-//       planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
-//
-//       /*
-//        Plane estimation may extend the size of the plane, or combine previously detected
-//        planes into a larger one. In the latter case, `ARSCNView` automatically deletes the
-//        corresponding node for one plane, then calls this method to update the size of
-//        the remaining plane.
-//        */
-//       plane.width = CGFloat(planeAnchor.extent.x)
-//       plane.height = CGFloat(planeAnchor.extent.z)
-//   }
-//   
-//   func renderer(_: SCNSceneRenderer, updateAtTime _: TimeInterval) {
-//    // 1. Check Our Frame Is Valid & That We Have Received Our Raw Feature Points
-//       guard let currentFrame = self.augmentedRealitySession.currentFrame,
-//           let featurePointsArray = currentFrame.rawFeaturePoints?.points else { return }
-//       voxelMap.addVoxels(featurePointsArray)
-//   }
-//
-//
-//}
+extension ViewController: ARSCNViewDelegate {
+   // MARK: - ARSCNViewDelegate
+
+   // Override to create and configure nodes for anchors added to the view's session.
+   func renderer(_: SCNSceneRenderer, nodeFor _: ARAnchor) -> SCNNode? {
+       // if planesVizNodes.count > 0 { return nil; }
+       let node = SCNNode()
+       return node
+   }
+
+   func renderer(_: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+       guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    
+        voxelMap.updateGroundPlane(planeAnchor)
+
+       // Create a SceneKit plane to visualize the plane anchor using its position and extent.
+       let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+       plane.firstMaterial?.diffuse.contents = UIColor.magenta
+
+       let planeNode = SCNNode(geometry: plane)
+       planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+
+       planesVizNodes[anchor.identifier] = node // keep track of plane nodes so you can move them once you localize to a new map.
+
+       /*
+        `SCNPlane` is vertically oriented in its local coordinate space, so
+        rotate the plane to match the horizontal orientation of `ARPlaneAnchor`.
+        */
+       planeNode.eulerAngles.x = -.pi / 2
+
+       // Make the plane visualization semitransparent to clearly show real-world placement.
+       // planeNode.opacity = 0.012
+       planeNode.opacity = 0.12
+
+       /*
+        Add the plane visualization to the ARKit-managed node so that it tracks
+        changes in the plane anchor as plane estimation continues.
+        */
+       node.addChildNode(planeNode)
+//       voxelMap.updateGroundPlane(planeAnchor)
+   }
+
+   func renderer(_: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+       // Update content only for plane anchors and nodes matching the setup created in `renderer(_:didAdd:for:)`.
+       guard let planeAnchor = anchor as? ARPlaneAnchor,
+           let planeNode = node.childNodes.first,
+           let plane = planeNode.geometry as? SCNPlane
+       else { return }
+       
+//       voxelMap.updateGroundPlane(planeAnchor)
+
+       // Plane estimation may shift the center of a plane relative to its anchor's transform.
+       planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+
+       /*
+        Plane estimation may extend the size of the plane, or combine previously detected
+        planes into a larger one. In the latter case, `ARSCNView` automatically deletes the
+        corresponding node for one plane, then calls this method to update the size of
+        the remaining plane.
+        */
+       plane.width = CGFloat(planeAnchor.extent.x)
+       plane.height = CGFloat(planeAnchor.extent.z)
+   }
+   
+   func renderer(_: SCNSceneRenderer, updateAtTime _: TimeInterval) {
+    // 1. Check Our Frame Is Valid & That We Have Received Our Raw Feature Points
+       guard let currentFrame = self.augmentedRealitySession.currentFrame,
+           let featurePointsArray = currentFrame.rawFeaturePoints?.points else { return }
+       voxelMap.addVoxels(featurePointsArray)
+   }
+
+
+}
 
 
